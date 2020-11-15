1c2663da511cecb205867a282fa76718b8691900
antpngl92 <nyagolov.anton@gmail.com>
2020-11-15 03:39:30 +0000

    added health/ path in urls.py

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
     path('', include('news.urls')),
     path('account/', include('account.urls')),
+    path('health/', lambda request: HttpResponse("OK")),
 ]
